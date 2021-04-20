f453335116e8f0d32106b8085dbe6fb9e2e4e060
Brunomri <19270033+Brunomri@users.noreply.github.com>
2021-04-20 10:27:28 -0300

    Add cars-create component

---
+++ src/app/components/views/cars/cars-create/cars-create.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {CarsService} from '../cars.service';
+import {CarsModel} from '../cars.model';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-cars-create',
+  templateUrl: './cars-create.component.html',
+  styleUrls: ['./cars-create.component.css']
+})
+export class CarsCreateComponent implements OnInit {
+
+  car: CarsModel = {
+    color: '', fuel: '', make: '', model: '', transmission: '', trim: '', year: ''
+  };
+
+  constructor(private service: CarsService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createCar(): void {
+    this.service.createCar(this.car).subscribe(() => {
+      console.log('New car created successfully:');
+      console.log(this.car);
+      this.router.navigate(['cars']);
+      this.service.message('New car created!');
+    }); /*, error => {
+      for(let i = 0; i < error.error.errors.length; i++) {
+        this.service.message(error.error.errors[i].message);
+      }
+    });*/
+  }
+}
--- src/app/components/views/cars/cars-read/cars-read.component.ts
+++ src/app/components/views/cars/cars-read/cars-read.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CarsService} from '../cars.service';
 import {CarsModel} from '../cars.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cars-read',
@@ -13,7 +14,7 @@ export class CarsReadComponent implements OnInit {
 
   displayedColumns: string[] = ['carId', 'make', 'model', 'year', 'trim', 'color', 'transmission', 'fuel', 'actions'];
 
-  constructor(private service: CarsService) { }
+  constructor(private service: CarsService, private router: Router) { }
 
   ngOnInit(): void {
     this.findAllCars();
@@ -21,8 +22,13 @@ export class CarsReadComponent implements OnInit {
 
   findAllCars(): void {
     this.service.findAllCars().subscribe(response => {
+      console.log('Showing all cars:');
       console.log(response);
       this.cars = response;
     });
+  }
+
+  goToCarsCreate(): void {
+    this.router.navigate(['cars/create']);
   }
 }
